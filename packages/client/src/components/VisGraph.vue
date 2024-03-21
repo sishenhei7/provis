7438dbdae4d83f974aa83632f983fa8364cf4270
yangzhou <982505136@qq.com>
2024-03-21 09:31:19 +0800

    refactor(client): add vis-network

---
+++ packages/client/src/components/VisGraph.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { computed, onMounted, ref, watch } from 'vue'
+import type { Data, Options } from 'vis-network'
+import { Network } from 'vis-network'
+
+defineProps({
+  msg: String,
+})
+
+const data = computed<Data>(() => ({}))
+const container = ref<HTMLElement>()
+const selected = ref<any>()
+
+onMounted(() => {
+  const options: Options = {
+    nodes: {
+      shape: 'dot',
+      size: 16,
+    },
+    physics: {
+      repulsion: {
+        centralGravity: 0.7,
+        springLength: 100,
+        springConstant: 0.01,
+      },
+      maxVelocity: 146,
+      solver: 'forceAtlas2Based',
+      timestep: 0.35,
+      stabilization: {
+        enabled: true,
+        iterations: 200,
+      },
+    },
+    groups: {
+      user: {
+        color: '#42b883',
+      },
+      unknown: {
+        color: '#b86542',
+      },
+      lib: {
+        color: '#b4b842',
+      },
+      page: {
+        color: '#42b2b8',
+      },
+      layout: {
+        color: '#4256b8',
+      },
+    },
+  }
+
+  const network = new Network(container.value!, data.value, options)
+
+  network.on('click', (e) => {
+    const id = e.nodes?.[0]
+    const node = (data.value.nodes as any[])?.find(i => i.id === id)?.extra
+    if (node)
+      selected.value = node
+      // console.log(111, node)
+  })
+
+  watch(data, () => {
+    network.setData(data.value)
+  })
+})
+</script>
+
+<template>
+  <div ref="container" class="vis-graph">
+    vis-graph
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
