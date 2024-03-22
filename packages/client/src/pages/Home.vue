904aa466a8b3b2336b59170b227a9e530ac07d39
yangzhou <982505136@qq.com>
2024-03-22 08:45:08 +0800

    refactor(client): add ts and vue-router

---
+++ packages/client/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  methods: {
+    goToAbout() {
+      this.$router.push('/about')
+    },
+  },
+}
+</script>
+
+<template>
+  <h2>HomeView</h2>
+  <button @click="goToAbout">
+    Go to About
+  </button>
+</template>
---
+++ packages/client/src/router.ts
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomeView from './pages/Home.vue'
+import AboutView from './pages/About.vue'
+
+const routes = [
+  { path: '/', component: HomeView },
+  { path: '/about', component: AboutView },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
