a2054c72e2aeda01917e9321d24d58066fa2501c
yangzhou <982505136@qq.com>
2024-03-25 00:01:56 +0800

    refactor(client): add search bar

--- packages/client/src/components/SearchBar.vue
+++ packages/client/src/components/SearchBar.vue
@@ -1,20 +1,31 @@
 <script setup>
 import { SearchOutline } from '@vicons/ionicons5'
+
+defineOptions({
+  inheritAttrs: false,
+})
 </script>
 
 <template>
   <div class="search-bar">
-    <n-button>naive-ui</n-button>
-    <n-input placeholder="搜索">
+    <n-input v-bind="$attrs" type="text" placeholder="搜索">
       <template #prefix>
         <n-icon :component="SearchOutline" />
       </template>
     </n-input>
+    <div class="search-bar-btns">
+      <slot />
+    </div>
   </div>
 </template>
 
 <style scoped>
 .search-bar {
+  display: flex;
+  align-items: center;
+}
 
+.search-bar-btns {
+  margin-left: 4px;
 }
 </style>
--- packages/client/src/pages/Components.vue
+++ packages/client/src/pages/Components.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref, watch } from 'vue'
+import { ListCircleOutline, LogoTableau } from '@vicons/ionicons5'
 import SearchBar from '../components/SearchBar.vue'
 
 // const search = computed({
@@ -9,9 +11,30 @@ import SearchBar from '../components/SearchBar.vue'
 //     router.replace({ query: { search } })
 //   },
 // })
+
+// todo: ts types
+const type = ref('list')
+function toggleType() {
+  type.value = type.value === 'list' ? 'graph' : 'list'
+}
+
+const searchString = ref('')
+watch(searchString, (newVal) => {
+  // eslint-disable-next-line no-console
+  console.log(newVal)
+})
 </script>
 
 <template>
   <h2>AboutView</h2>
-  <SearchBar />
+  <SearchBar v-model:value="searchString">
+    <n-button quaternary circle type="primary" @click="toggleType()">
+      <template #icon>
+        <n-icon :size="28">
+          <ListCircleOutline v-show="type === 'list'" />
+          <LogoTableau v-show="type === 'graph'" />
+        </n-icon>
+      </template>
+    </n-button>
+  </SearchBar>
 </template>
